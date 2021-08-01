df95b8af19468d5c6e3aa3c03f4c73a1b07eff15
CyberObiOne <hiva.vlad@gmail.com>
2021-08-02 02:24:48 +0300

    Update metric.go

--- _chains/_osmosis/exporter/metric/metric.go
+++ _chains/_osmosis/exporter/metric/metric.go
@@ -3,7 +3,7 @@ package metric
 import (
 	"go.uber.org/zap"
 
-	#rest "github.com/node-a-team/Cosmos-IE/chains/osmosis/getData/rest"
+//	rest "github.com/node-a-team/Cosmos-IE/chains/osmosis/getData/rest"
 	rest "github.com/CyberObiOne/Cosmos-IE/chains/osmosis/getData/rest"
 //	rpc "github.com/node-a-team/Cosmos-IE/chains/osmosis/getData/rpc"
 	utils "github.com/CyberObiOne/Cosmos-IE/utils"
