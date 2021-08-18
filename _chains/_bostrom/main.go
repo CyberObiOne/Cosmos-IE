57bdd1676ef80865aa0d9c2bf1a42ffb9e599e91
CyberObiOne <hiva.vlad@gmail.com>
2021-08-18 21:55:53 +0300

    Create main.go

---
+++ _chains/_bostrom/main.go
@@ -0,0 +1,54 @@
+package bostrom
+
+import (
+	"fmt"
+	"net/http"
+	"go.uber.org/zap"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+//	"github.com/node-a-team/Cosmos-IE/chains/osmosis/exporter"
+	"github.com/CyberObiOne/Cosmos-IE/chains/osmosis/exporter"
+)
+
+const (
+	bech32MainPrefix = "bostrom"
+	
+		
+)
+var (
+	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+	Bech32PrefixAccPub = Bech32PrefixAccAddr + "pub"
+	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+	Bech32PrefixValAddr = Bech32PrefixAccAddr + "valoper"
+	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+	Bech32PrefixValPub = Bech32PrefixAccAddr + "valoperpub"
+	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+	Bech32PrefixConsAddr = Bech32PrefixAccAddr + "valcons"
+	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+	Bech32PrefixConsPub = Bech32PrefixAccAddr + "valconspub"
+)	
+func Main(port string) {
+
+	log,_ := zap.NewDevelopment()
+        defer log.Sync()
+
+	config := sdk.GetConfig()
+	config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+	config.Seal()
+		
+	http.Handle("/metrics", promhttp.Handler())
+	go exporter.Start(log)
+
+	err := http.ListenAndServe(":" +port, nil)
+	// log
+        if err != nil {
+                // handle error
+                log.Fatal("HTTP Handle", zap.Bool("Success", false), zap.String("err", fmt.Sprint(err),))
+        } else {
+		log.Info("HTTP Handle", zap.Bool("Success", true), zap.String("err", "nil"), zap.String("Listen&Serve", "Prometheus Handler(Port: " +port +")"),)
+        }
+}
